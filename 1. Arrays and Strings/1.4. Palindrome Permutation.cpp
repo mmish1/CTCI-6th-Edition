aead88e8943f5360edee9c936457b4dae8fd9bb1
Manisha Mishra <manisha0603a@gmail.com>
2017-11-20 11:48:11 -0500

    Update 1.4. Palindrome Permutation.cpp

--- 1. Arrays and Strings/1.4. Palindrome Permutation.cpp
+++ 1. Arrays and Strings/1.4. Palindrome Permutation.cpp
@@ -3,16 +3,39 @@
 		phrase that is the same forwards and backwards. A permutation is a 
 		rearrangement of letters. The palindrome does not need to be limited to just dictionary words. 
 
-		EXAMPLE Input: Tact Coa Output: True 
+		EXAMPLE Input: Tact Coa 
+				Output: True 
 				(permutations: "taco cat", "atco eta", etc.)*/
 
 
 #include<iostream>
 #include<cstring>
 #include<string>
+#include<map>
 
 using namespace std;
 
+/*Method 1: using map*/
+bool isPalindromePermutation(string s, bool res) {
+	map<char,int> m;
+	int nOddChar=0;
+	for(int i=0;i<s.length();i++){
+		if(s[i]!=' ') m[tolower(s[i])]++;
+	}
+	
+	for(map<char,int>::iterator it=m.begin();it!=m.end();it++){
+		if((it->second)%2!=0 ){
+			nOddChar++;
+			if(nOddChar>1) {
+				res=false;
+				break;
+			}
+		}
+	}
+	return res;
+}
+
+/*Method 2: using bactracking*/
 bool isPalindrome(string s){
 	int i=0,j=s.length()-1;
 	while(i<=j){
@@ -51,7 +74,14 @@ bool palindromePermutation(string s){
 }
 
 main(){
+	cout<<"Is taco cati the permutation of a palindrome? Using Backtracking: ";
 	if(palindromePermutation("taco cati")){
+		cout<<"yes"<<endl;
+	}
+	else cout<<"no"<<endl;
+	
+	cout<<"Is Tact Coa the permutation of a palindrome? Using map: ";	
+	if(isPalindromePermutation("Tact Coa",true)){
 		cout<<"yes"<<endl;
 	}
 	else cout<<"no"<<endl;
