33dd0fe0a22e3634230aa2411d7a6e314faedd48
Manisha Mishra <manisha0603a@gmail.com>
2017-11-20 12:01:46 -0500

    Update 1.9. String Rotation.cpp

--- 1. Arrays and Strings/1.9. String Rotation.cpp
+++ 1. Arrays and Strings/1.9. String Rotation.cpp
@@ -1,4 +1,4 @@
-/*Q1.9) String Rotation:Assumeyou have a method isSubstringwhich checks ifoneword is 
+/*Q1.9) String Rotation: Assumeyou have a method isSubstring which checks if one word is 
 		a substring of another. Given two strings, sl and s2, write code to check if 
 		s2 is a rotation of sl using only one call to isSubstring 
 		(e.g., "waterbottle" is a rotation of"erbottlewat"). */
@@ -7,7 +7,7 @@
 #include<string>
 using namespace std;
 
-bool rotateString(string s1, string s2){
+bool isSubstring(string s1, string s2){
 	if(s1.length()!=s2.length()) return false;
 	if(s1==s2) return true;
 	string c=s1+s1;
@@ -16,10 +16,17 @@ bool rotateString(string s1, string s2){
 }
 
 main(){
-	if(rotateString("waterbotwle","erbotwlewat")) cout<<"yes"<<endl;
+	string s1,s2;
+	s1="waterbotwle";
+	s2="erbotwlewat";
+	cout<<"Is "<<s1<<" a rotation of "<<s2<<"? ";
+	if(isSubstring(s1,s2)) cout<<"yes"<<endl;
 	else cout<<"no"<<endl;
 	
-	if(rotateString("waterbotwle","arbotwlewat")) cout<<"yes"<<endl;
+	s1="waterbotwle";
+	s2="arbotwlewat";
+	cout<<"Is "<<s1<<" a rotation of "<<s2<<"? ";
+	if(isSubstring(s1,s2)) cout<<"yes"<<endl;
 	else cout<<"no"<<endl;
 	
 	return 0;
